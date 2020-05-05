4b0fa9f69191b7b36ced60a1a07b87294899f75c
GP Buffo <gp@labs.epiuse.com>
2020-05-05 22:12:03 +0200

    Add initial image container

---
+++ src/components/ImageContainer.vue
@@ -0,0 +1,30 @@
+<template>
+  <canvas id="canvas" width="1280" height="720"></canvas>
+</template>
+
+<script>
+export default {
+  name: "ImageContainer",
+  mounted() {
+    this.loadImage();
+  },
+  methods: {
+    loadImage() {
+      const canvas = document.getElementById("canvas");
+      const context = canvas.getContext("2d");
+      const image = new Image();
+
+      image.src = "https://picsum.photos/1280/720";
+      image.onload = () => {
+        context.drawImage(image, 0, 0);
+      };
+    }
+  }
+};
+</script>
+
+<style scoped>
+#canvas {
+  border: 1px solid black;
+}
+</style>
