ab058f54c7532d9ec8a9f4fe26a4594bbbb5b5ca
GP Buffo <gp@labs.epiuse.com>
2020-05-12 22:10:44 +0200

    Add boilerplate for server API

---
+++ src/api/api.js
@@ -0,0 +1,15 @@
+// TODO: Add store
+
+export const loadTags = () => [];
+
+export const loadBoundingBoxes = () => [];
+
+export const saveBoundingBox = (boundingBox) => {
+  console.log(boundingBox);
+};
+
+export const deleteBoundingBox = (boundingBox) => {
+  console.log(boundingBox);
+};
+
+export default {};
