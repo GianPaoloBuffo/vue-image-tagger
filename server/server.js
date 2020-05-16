10a2fe8623a3e548675c4db0763b5e7c5192fcc4
GP Buffo <gp@labs.epiuse.com>
2020-05-16 13:46:41 +0200

    Add Express server

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const db = require('./app/model');
+db.sequelize.sync();
+
+const app = express();
+
+// TODO: Do I need CORS?
+const corsOptions = {
+    origin: 'http://localhost:8082',
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+   res.json({ message: 'Vue Image Tagger' });
+});
+
+require('./app/routes/tag.routes')(app);
+require('./app/routes/boundingBox.routes')(app);
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
