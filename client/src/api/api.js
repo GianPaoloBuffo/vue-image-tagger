946e6a156d67ebb093334b6f5638ff12fd032b3e
GP Buffo <gp@labs.epiuse.com>
2020-05-16 13:57:31 +0200

    Add Axios to client API

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,15 +1,11 @@
-export const loadTags = () => [{ id: 1, text: 'dog' }];
+import http from './config';
 
-export const loadBoundingBoxes = () => [{
-  id: 1,
-  top: 100,
-  left: 100,
-  height: 50,
-  width: 50,
-}];
+export const loadTags = () => http.get('/tags');
 
-export const saveBoundingBox = (boundingBox, tag) => ({ ...boundingBox, tag });
+export const loadBoundingBoxes = () => http.get('/boundingBoxes');
 
-export const deleteBoundingBox = (boundingBox) => boundingBox;
+export const saveBoundingBox = (boundingBox) => http.put(`/boundingBoxes/${boundingBox.id}`, boundingBox);
+
+export const deleteBoundingBox = (boundingBox) => http.delete(`/boundingBoxes/${boundingBox.id}`);
 
 export default {};
