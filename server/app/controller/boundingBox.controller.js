9fb39ae9f7e8df1f120c39fae10dcdb80b355a73
GP Buffo <gp@labs.epiuse.com>
2020-05-18 21:23:38 +0200

    Reduce duplication in controllers

--- server/app/controller/boundingBox.controller.js
+++ server/app/controller/boundingBox.controller.js
@@ -2,7 +2,15 @@ const db = require('../model');
 const Tag = db.tag;
 const BoundingBox = db.boundingBox;
 
-exports.create = (req, res) => {
+const getUpdatedTags = (req) => {
+    return db.sequelize.transaction(transaction => {
+        return Promise.all(req.body.tags.map(tag => {
+            return Tag.findOrCreate({ where: { label: tag.label }, transaction });
+        }));
+    });
+}
+
+exports.create = async (req, res) => {
     if (!req.body.top || !req.body.left || !req.body.width || !req.body.height) {
         res.status(400).send({
             message: 'The top, left, width and height properties cannot be empty',
@@ -10,48 +18,18 @@ exports.create = (req, res) => {
         return;
     }
 
-    // TODO: Duplicated
-    let updatedTags = [];
-
-    req.body.tags.forEach(tag => {
-        Tag.findByPk(tag.id)
-        .then(existingTag => {
-            // If no existing tag
-            let updatedTag;
-            if (!existingTag) {
-                // Create new tag
-                Tag.create({ label: tag.label })
-                .then(data => {
-                    updatedTag = data;
-                })
-                .catch(err => {
-                    res.status(500).send({
-                        message: err.message || 'Error while creating tag',
-                    });
-                });
-            } else {
-                updatedTag = existingTag;
-            }
-
-            updatedTags.push(updatedTag);
-        })
-        .catch(err => {
-            res.status(500).send({
-                message: err.message || 'Error while fetching tag',
-            });
-        });
-    });
+    const updatedTags = await getUpdatedTags(req);
 
     const boundingBox = {
         top: req.body.top,
         left: req.body.left,
         width: req.body.width,
         height: req.body.height,
-        tags: updatedTags,
     };
 
     BoundingBox.create(boundingBox)
         .then(data => {
+            data.setTags(updatedTags.map(tag => tag[0]));
             res.send(data);
         })
         .catch(err => {
@@ -60,7 +38,7 @@ exports.create = (req, res) => {
 };
 
 exports.findAll = (req, res) => {
-    BoundingBox.findAll()
+    BoundingBox.findAll({ include: { model: Tag, as: 'tags' } })
         .then(data => {
             res.send(data);
         })
@@ -69,37 +47,8 @@ exports.findAll = (req, res) => {
         });
 };
 
-exports.update = (req, res) => {
-    let updatedTags = [];
-
-    req.body.tags.forEach(tag => {
-        Tag.findByPk(tag.id)
-            .then(existingTag => {
-                // If no existing tag
-                let updatedTag;
-                if (!existingTag) {
-                    // Create new tag
-                    Tag.create({ label: tag.label })
-                        .then(data => {
-                            updatedTag = data;
-                        })
-                        .catch(err => {
-                            res.status(500).send({
-                                message: err.message || 'Error while creating tag',
-                            });
-                        });
-                } else {
-                    updatedTag = existingTag;
-                }
-
-                updatedTags.push(updatedTag);
-            })
-            .catch(err => {
-                res.status(500).send({
-                    message: err.message || 'Error while fetching tag',
-                });
-            });
-    });
+exports.update = async (req, res) => {
+    const updatedTags = await getUpdatedTags(req);
 
     BoundingBox.findByPk(req.params.id)
         .then(boundingBox => {
@@ -113,8 +62,9 @@ exports.update = (req, res) => {
                 left: req.body.left,
                 width: req.body.width,
                 height: req.body.height,
-                tags: updatedTags,
-            })
+            });
+
+            boundingBox.setTags(updatedTags.map(tag => tag[0]));
 
             res.send(boundingBox);
         })
--- server/app/controller/tag.controller.js
+++ server/app/controller/tag.controller.js
@@ -1,25 +1,6 @@
 const db = require('../model');
 const Tag = db.tag;
 
-// exports.create = (req, res) => {
-//     if (!req.body.label) {
-//         res.status(400).send({ message: 'Label cannot be empty' });
-//         return;
-//     }
-//
-//     const tag = {
-//         label: req.body.label,
-//     };
-//
-//     Tag.create(tag)
-//         .then(data => {
-//             res.send(data);
-//         })
-//         .catch(err => {
-//            res.status(500).send({ message: err.message || 'Error while creating tag' });
-//         });
-// };
-
 exports.findAll = (req, res) => {
     Tag.findAll()
         .then(data => {
